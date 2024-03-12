caef6182b204cb7bb0b003c7ee0ef199ce8a5e76
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2024-03-12 13:37:21 -0500

    Update Flag.mjs

--- Stream Tool/Resources/Scripts/GUI/Flag.mjs
+++ Stream Tool/Resources/Scripts/GUI/Flag.mjs
@@ -1,30 +1,26 @@
 import { stPath } from './Globals.mjs';
 import { getJson } from './File System.mjs';
 
-const roundList = await getJson(stPath.text + "/Flag Names");
+const flagList = await getJson(stPath.text + "/Flag Names");
 
 class Flag {
 
-    #flagInp = document.getElementById('flagName');
     #flagSelect = document.getElementById('flagNameSelect');
 
     
     constructor() {
         
-        // create the round select list
-        for (let i = 0; i < roundList.length; i++) {
+        // create the flag select list
+        for (let i = 0; i < flagList.length; i++) {
 
-            const roundOption = document.createElement('option');
-            roundOption.value = roundList[i].name;
-            roundOption.innerHTML = roundList[i].name;
+            const flagOption = document.createElement('option');
+            flagOption.value = flagList[i].name;
+            flagOption.innerHTML = flagList[i].name;
 
             // add colors to the list
-            roundOption.style.backgroundColor = "var(--bg5)";
-            if (roundList[i].showNumber) {
-                roundOption.style.backgroundColor = "var(--bg2)";
-            }
-
-            this.#flagSelect.appendChild(roundOption);
+            flagOption.style.backgroundColor = "var(--bg5)";
+            
+            this.#flagSelect.appendChild(flagOption);
 
         }
 
@@ -46,4 +42,4 @@ class Flag {
 
 }
 
-export const round = new Flag;
+export const flag = new Flag;
